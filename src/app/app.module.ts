b7b0968b4d64979998c16440e118365f22ee0a12
huysonny <huy26072003h@gmail.com>
2024-01-12 22:39:56 +0700

    order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrderComponent,
+    OrderConfirmComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [HomeComponent],
+  bootstrap: [
+    //HomeComponent
+    // OrderComponent,
+    OrderConfirmComponent,
+  ],
 })
 export class AppModule {}
